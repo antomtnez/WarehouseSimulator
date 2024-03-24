11d619ddeb0a6a3026e5ff6e5e13cad81408c22a
Anto Martínez <antomartinezdev@gmail.com>
2024-03-24 13:29:09 +0100

    I update Order View to manage better the order list show in UI

--- Assets/Scripts/UI/OrderView.cs
+++ Assets/Scripts/UI/OrderView.cs
@@ -6,16 +6,49 @@ public class OrderView : MonoBehaviour
 {
     [SerializeField] GameObject itemBoxPrefab;
     [SerializeField] GameObject itemBoxListPanel;
+    private List<ItemBoxView> m_ItemBoxViewPool = new List<ItemBoxView>();
     private Dictionary<string, ItemBoxView> m_ItemBoxViewDictionary = new Dictionary<string, ItemBoxView>();
     [SerializeField] TextMeshProUGUI rewardText;
 
+    void Awake(){
+        InitializePool();
+    }
+
+    void InitializePool(){
+        for(int i=0; i < 3; i++){
+            AddItemBoxToPool();
+        }
+    }
+
+    ItemBoxView AddItemBoxToPool(){
+        ItemBoxView itemBoxView = Instantiate(itemBoxPrefab, itemBoxListPanel.transform).GetComponent<ItemBoxView>();
+        m_ItemBoxViewPool.Add(itemBoxView);
+        itemBoxView.gameObject.SetActive(false);
+        return itemBoxView;
+    }
+
+    ItemBoxView GetItemBoxFromPool(){
+        foreach(ItemBoxView itemBoxView in m_ItemBoxViewPool)
+            if(!itemBoxView.gameObject.activeInHierarchy)
+                return itemBoxView;
+        
+        return AddItemBoxToPool();
+    }
+
+    void ResetPool(){
+        foreach(ItemBoxView itemBoxView in m_ItemBoxViewPool)
+            itemBoxView.gameObject.SetActive(false);
+    }
+
     public void SetOrderItems(List<Order.OrderEntry> orderEntry){
         m_ItemBoxViewDictionary.Clear();
-
+        ResetPool();
+        
         for(int i=0; i < orderEntry.Count; i++){
-            ItemBoxView itemBoxView = Instantiate(itemBoxPrefab, itemBoxListPanel.transform).GetComponent<ItemBoxView>();
+            ItemBoxView itemBoxView = GetItemBoxFromPool();
             itemBoxView.SetItemIcon(WarehouseStorage.Instance.ItemDB.GetItem(orderEntry[i].ItemId));
             itemBoxView.SetItemText(orderEntry[i].CurrentStock, orderEntry[i].OrderedStock);
+            itemBoxView.gameObject.SetActive(true);
             m_ItemBoxViewDictionary.Add(orderEntry[i].ItemId, itemBoxView);
         }
     }
