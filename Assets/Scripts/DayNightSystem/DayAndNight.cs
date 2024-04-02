dd0de3edcc3c46428dac381d1ecfba13decaf70d
Anto Martínez <antomartinezdev@gmail.com>
2024-04-02 13:01:04 +0200

    I create some scripts to show day info to the player and a new system to define game sprints by days

--- Assets/Scripts/DayNightSystem/DayAndNight.cs
+++ Assets/Scripts/DayNightSystem/DayAndNight.cs
@@ -11,18 +11,28 @@ public class DayAndNight : MonoBehaviour
     [SerializeField] Light directionalLight;
 
     [Header("Day Variables")]
-    [SerializeField] float m_DayDurationInSeconds = 60f;
-
+    [SerializeField] int m_Day = 1;
+    [SerializeField] float m_DayDurationInSeconds;
     [SerializeField] float m_CurrentTime = 0f;
+    public int Day => m_Day;
+    public float DayDuration => m_DayDurationInSeconds;
+    public float CurrentTime => m_CurrentTime;
+    private DayTimePresenter m_DayTimePresenter;
+
+    void Start(){
+        m_DayTimePresenter = new DayTimePresenter(this, GameObject.FindObjectOfType<DayTimeView>());
+    }
 
     void Update(){
         UpdateTime();
         UpdateDayAndNightCycle();
+        if(IsDayFinished())
+            FinishDay();
     }
 
     void UpdateTime(){
         m_CurrentTime += Time.deltaTime / m_DayDurationInSeconds;
-        m_CurrentTime = Mathf.Repeat(m_CurrentTime, 1f);
+        m_DayTimePresenter.SetTime();
     }
 
     void UpdateDayAndNightCycle(){
@@ -34,6 +44,17 @@ public class DayAndNight : MonoBehaviour
         directionalLight.color = directionalLightGradient.Evaluate(m_CurrentTime);
     }
 
+    bool IsDayFinished(){
+        return m_CurrentTime >= 1;
+    }
 
+    void FinishDay(){
+        
+    }
 
+    void StartNewDay(){
+        m_CurrentTime = 0;
+        m_Day++;
+        m_DayTimePresenter.SetDay();
+    }
 }
