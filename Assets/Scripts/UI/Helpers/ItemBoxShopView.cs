3dce463372e350e379ef9a59b736a06c42214a09
Anto Martínez <antomartinezdev@gmail.com>
2024-04-02 14:26:10 +0200

    I change the shop UI to only have access when player finish the day

--- Assets/Scripts/UI/Helpers/ItemBoxShopView.cs
+++ Assets/Scripts/UI/Helpers/ItemBoxShopView.cs
@@ -5,11 +5,14 @@ using UnityEngine.UI;
 
 public class ItemBoxShopView : MonoBehaviour
 {
+    [Header("Item data")]
     [SerializeField] Image ItemIcon;
     [SerializeField] TextMeshProUGUI ItemPriceText;
+    [Header("Amount to buy controls")]
     [SerializeField] TextMeshProUGUI ItemAmountText;
     [SerializeField] Button AddAmountButton;
     [SerializeField] Button RestAmountButton;
+    [SerializeField] TextMeshProUGUI ItemAmountCostText;
     private string m_ItemId;
     private int m_ItemAmount;
     public int ItemAmountToBuy => m_ItemAmount;
@@ -32,28 +35,35 @@ public class ItemBoxShopView : MonoBehaviour
     public void SetItemInfo(Item item){
         ItemIcon.sprite = item.Icon;
         m_ItemId = item.Id;
-        ItemPriceText.SetText($"{item.BuyingPrice},00");
+        ItemPriceText.SetText($"{item.BuyingPrice},00 UD");
         CheckToBlockAmountButtons();
     }
 
     public void Reset(){
         m_ItemAmount = 0;
         SetAmountToBuy();
+        SetMoneyAmountToCost();
         CheckToBlockAmountButtons();
     }
 
     void AddAmountToBuy(){
         m_ItemAmount++;
         SetAmountToBuy();
+        SetMoneyAmountToCost();
     }
 
     void RestAmountToBuy(){
         m_ItemAmount--;
         SetAmountToBuy();
+        SetMoneyAmountToCost();
     }
 
     void SetAmountToBuy(){
         ItemAmountText.SetText($"{m_ItemAmount}");
+    }
+
+    void SetMoneyAmountToCost(){
+        ItemAmountCostText.SetText($"{m_ItemAmount * WarehouseStorage.Instance.ItemDB.GetItem(m_ItemId).BuyingPrice}");
     }
 
     void CheckToBlockAmountButtons(){
